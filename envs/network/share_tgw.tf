e2e58008e59a4f2bf4032d06adf4279dcebe2197
Aarash Samimi <aarash.samimi@cdw.com>
2025-10-07 12:48:44 -0700

    Added TF script to share TGW

---
+++ envs/network/share_tgw.tf
@@ -0,0 +1,22 @@
+variable "dev_account_id"  { type = string }
+variable "prod_account_id" { type = string }
+
+resource "aws_ram_resource_share" "tgw" {
+  name                      = "tgw-share"
+  allow_external_principals = false
+}
+
+resource "aws_ram_principal_association" "dev" {
+  resource_share_arn = aws_ram_resource_share.tgw.arn
+  principal          = var.dev_account_id
+}
+
+resource "aws_ram_principal_association" "prod" {
+  resource_share_arn = aws_ram_resource_share.tgw.arn
+  principal          = var.prod_account_id
+}
+
+resource "aws_ram_resource_association" "tgw" {
+  resource_share_arn = aws_ram_resource_share.tgw.arn
+  resource_arn       = module.tgw.tgw_arn   # make sure tgw_arn is an output
+}
