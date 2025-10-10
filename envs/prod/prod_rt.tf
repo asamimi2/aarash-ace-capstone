2ea271fdf6ea3f39b3d6f4d52edd7a809a2d01cb
Aarash Samimi <aarash.samimi@cdw.com>
2025-10-10 12:07:44 -0700

    propagated dev and prod to tgw and added routes between prod and dev

---
+++ envs/prod/prod_rt.tf
@@ -0,0 +1,5 @@
+resource "aws_route" "prod_to_dev" {
+  route_table_id         = module.vpc.private_route_table_id
+  destination_cidr_block = "10.10.0.0/16" # DEV CIDR
+  transit_gateway_id     = data.terraform_remote_state.network.outputs.tgw_id
+}
