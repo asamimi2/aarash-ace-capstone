042c288942a5174a0447c3bc739a276adeca4df2
Aarash Samimi <aarash.samimi@cdw.com>
2025-10-13 15:24:31 -0700

    updated web server for prod and dev

---
+++ envs/dev/outputs.tf
@@ -0,0 +1,10 @@
+output "dev_private_ip" {
+  description = "Private IP of the Dev web instance"
+  value       = aws_instance.dev_web.private_ip
+}
+
+# (Optional but handy)
+output "dev_instance_id" {
+  description = "Instance ID of the Dev web instance"
+  value       = aws_instance.dev_web.id
+}
--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -36,3 +36,67 @@ resource "aws_route" "public_internet" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.prod_igw.id
 }
+
+# Security Group
+resource "aws_security_group" "prod_web_sg" {
+  name   = "prod-web-sg"
+  vpc_id = module.vpc.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]     # public access
+  }
+  ingress {
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["10.10.0.0/16"]  # allow ping from Dev
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  tags = { Name = "prod-web-sg" }
+}
+
+# IAM Role for SSM
+resource "aws_iam_role" "prod_ssm_role" {
+  name = "prod-ssm-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = { Service = "ec2.amazonaws.com" }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+resource "aws_iam_role_policy_attachment" "prod_ssm_attach" {
+  role       = aws_iam_role.prod_ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+resource "aws_iam_instance_profile" "prod_ssm_profile" {
+  name = "prod-ssm-profile"
+  role = aws_iam_role.prod_ssm_role.name
+}
+
+# EC2 instance (public)
+resource "aws_instance" "prod_web" {
+  ami                    = "ami-0c7217cdde317cfec"
+  instance_type          = "t3.micro"
+  subnet_id              = module.vpc.public_subnet_ids[0]
+  vpc_security_group_ids = [aws_security_group.prod_web_sg.id]
+  iam_instance_profile   = aws_iam_instance_profile.prod_ssm_profile.name
+  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y httpd
+              systemctl start httpd
+              systemctl enable httpd
+              echo "Hello from PROD Web Server" > /var/www/html/index.html
+              EOF
+  tags = { Name = "prod-web" }
+}
